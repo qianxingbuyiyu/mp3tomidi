81e676dfc7ccfc907dabc8bbfbcddad10344666b
qianxingbuyiyu <3558993389@qq.com>
2026-09-16 16:22:49 +0800

    fix nativeCanvas imports

--- app/src/main/java/com/mp3tomidi/app/MainActivity.kt
+++ app/src/main/java/com/mp3tomidi/app/MainActivity.kt
@@ -23,6 +23,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.nativeCanvas
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
